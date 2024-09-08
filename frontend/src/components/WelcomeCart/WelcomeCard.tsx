2a827d096b2bf5e7cc1dfa2e1d304671c355e1f0
Michał Galiński <michalg359@gmail.com>
2024-09-08 20:10:23 +0200

    add new background and finished layout of welcome page

--- frontend/src/components/WelcomeCart/WelcomeCard.tsx
+++ frontend/src/components/WelcomeCart/WelcomeCard.tsx
@@ -1,9 +1,16 @@
+import { useEffect } from "react";
 import "./WelcomeCard.scss";
 
 const WelcomeCard: React.FC = () => {
+  useEffect(() => {
+    fetch("http://localhost:5000/")
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.log(err));
+  }, []);
   return (
     <div className="welcome-card-container">
-      <h1>Cashflow 📊</h1>
+      <h1>Cashflow </h1>
       <h3>
         Track your income, manage expenses, and maximize your savings
         effortlessly.

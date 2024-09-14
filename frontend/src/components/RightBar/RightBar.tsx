13391cc02d0fe078614a45ce26bceb2cee62fbc6
Michał Galiński <michalg359@gmail.com>
2024-09-14 13:33:57 +0200

    add doughnut in rightBar

--- frontend/src/components/RightBar/RightBar.tsx
+++ frontend/src/components/RightBar/RightBar.tsx
@@ -1,5 +1,7 @@
 import styles from "./RightBar.module.scss";
 
+import { Doughnut } from "../Doughnut/Dougnut";
+
 import { calcTotalAmount } from "../../services/calcTotalAmount";
 import { TransactionsProps } from "../MainContent/Transactions/Transactions";
 
@@ -9,7 +11,9 @@ export const RightBar: React.FC<TransactionsProps> = ({ transactions }) => {
       <div className={styles.wrapper}>
         <h4>Expenses by categories</h4>
         <div className={styles.piechart}>
-          <img src="/img/piechart.jpg" alt="Test image for piechart" />
+          {/* <img src="/img/piechart.jpg" alt="Test image for piechart" />
+           */}
+          <Doughnut />
         </div>
         <section>
           <h3>Total Expenses Value</h3>
